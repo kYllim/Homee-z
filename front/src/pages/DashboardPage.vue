3b12017b556528ead02a58e03d60b2a083357aea
Lytween Anne VICTOIRE <rosaes@carnelian.local>
2025-11-05 22:08:00 +0100

    v1 du front sans merge avec main

---
+++ front/src/pages/DashboardPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <p>Voici le calendrier :</p>
+    <Calendar />
+  </div>
+</template>
+
+<script setup>
+import Calendar from '@/components/Calendar.vue'
+</script>
---
+++ front/tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
