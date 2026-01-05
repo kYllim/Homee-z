3c9fe720c543ce9ca319ef729d41f4012e4548e4
Lytween Victoire <victoire.lytween@gmail.com>
2026-01-05 12:34:51 +0100

    corvées feature terminé

--- back/migrations/Version20251206164455.php
+++ back/migrations/Version20251206164455.php
@@ -7,10 +7,8 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20251206164455 extends AbstractMigration
+
+final class Version20251123175635 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,13 +17,12 @@ final class Version20251206164455 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        
+        $this->addSql('ALTER TABLE shopping_list ALTER household_id DROP NOT NULL');
+        $this->addSql('ALTER TABLE shopping_list ALTER creator_id DROP NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
     }
 }
